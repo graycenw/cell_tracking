887626d84ec07aabe1b7be1d1f84daa79e4ff386
Graycen Wheeler <52644726+graycenw@users.noreply.github.com>
2020-10-19 22:09:52 -0600

    remove all integers

--- TDScript.m
+++ TDScript.m
@@ -1,11 +1,25 @@
+clear all
+clc
+
+% Load TrackMate output
+%     Ultimately, we want to be able to import directly from an Excel
+%     document
 load("TrackingData.mat")
 ModifiedTrackingData;
-ModifiedTrackingData(ismember(ModifiedTrackingData.particledetectionx,'0'),:)=[];
-L=~ModifiedTrackingData.particledetectionx==0; %true non-zero L values
-L_toDelete=ModifiedTrackingData(L,:);
-ModifiedTrackingData(~L,:)=[]; %removes columns with L(0)
 
-ModifiedTrackingData(ismember(ModifiedTrackingData.particledetectiony,'0'),:)=[];
-M=~ModifiedTrackingData.particledetectiony==0; %true non-zero M values
-M_toDelete=ModifiedTrackingData(M,:);
-ModifiedTrackingData(~M,:)=[] %removes columns with M(0)
+% Remove tracked points where both the x and y coordinates are integers
+L=rem(ModifiedTrackingData.particledetectionx,1)~=0;
+M=rem(ModifiedTrackingData.particledetectiony,1)~=0;
+
+N=ones(size(L));
+for i=1:length(L)
+    if L(i,1)==0 && M(i,1)==0
+        N(i,1)=0;
+    end
+end
+
+ModifiedTrackingData(~N,:)=[];
+
+% Remove cells with fewer than the designated number of consecutive
+% timepoints tracked
+
