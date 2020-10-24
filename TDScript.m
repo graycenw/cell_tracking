f2a1a5e3ee5ea9300684fb68538e87415fdd0306
amritapurkayastha <73137068+amritapurkayastha@users.noreply.github.com>
2020-10-23 22:59:03 -0600

    Add files via upload

--- TDScript.m
+++ TDScript.m
@@ -1,19 +1,5 @@
-% comment
-
-clear
-clc
 
 load("TrackingData.mat")
-ModifiedTrackingData;
-ModifiedTrackingData(ismember(ModifiedTrackingData.particledetectionx,'0'),:)=[];
-L=~ModifiedTrackingData.particledetectionx==0; %true non-zero L values
-L_toDelete=ModifiedTrackingData(L,:);
-ModifiedTrackingData(~L,:)=[]; %removes columns with L(0)
-
-ModifiedTrackingData(ismember(ModifiedTrackingData.particledetectiony,'0'),:)=[];
-M=~ModifiedTrackingData.particledetectiony==0; %true non-zero M values
-M_toDelete=ModifiedTrackingData(M,:);
-ModifiedTrackingData(~M,:)=[]; %removes columns with M(0)
 
 Lremainders=rem(ModifiedTrackingData.particledetectionx,1);
 Ltrue=(Lremainders==0); 
@@ -22,3 +8,7 @@ ModifiedTrackingData(Ltrue,:)=[]; %removes columns with L(whole number)
 Mremainders=rem(ModifiedTrackingData.particledetectiony,1);
 Mtrue=(Mremainders==0);
 ModifiedTrackingData(Mtrue,:)=[]; %removes columns with M(whole number)
+
+x=12;
+TimePointTotals=ModifiedTrackingData.particlenSpots>=x;
+ModifiedTrackingData(~TimePointTotals,:)=[]; %removes data with less than x time points
