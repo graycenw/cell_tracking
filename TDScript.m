ce2ff28517f432d616a65eae0214c8f6997996ca
amritapurkayastha <73137068+amritapurkayastha@users.noreply.github.com>
2020-11-11 00:10:11 -0700

    Add files via upload

--- TDScript.m
+++ TDScript.m
@@ -1,3 +1,5 @@
+clc;clear; close all
+
 
 load("TrackingData.mat")
 
@@ -13,10 +15,38 @@ x=12;
 TimePointTotals=ModifiedTrackingData.particlenSpots>=x;
 ModifiedTrackingData(~TimePointTotals,:)=[]; %removes data with less than x time points
 
-Change=diff(ModifiedTrackingData.particledetectiont);
-TrueChange=Change<0;
-TrueChange(1)=1;
-%This makes it so that the true values are the first timepoints so we would
-%subtract those from the other values, but I don't know how to do that
+CellChange=ischange(ModifiedTrackingData.particleid); %finds changes in particleid
+ModifiedTrackingData.deltax=double(CellChange);
+ModifiedTrackingData.deltax(1)=1; 
 
+isChangexIND=[1;find(diff(ModifiedTrackingData.particleid))+1];
+for i=1:(length(isChangexIND)-1)
+    IND1=isChangexIND(i);
+    IND2=isChangexIND(i+1)-1;
+    
+    
+    ModifiedTrackingData.deltax(IND1:IND2)=ModifiedTrackingData.particledetectionx(IND1:IND2)-ModifiedTrackingData.particledetectionx(IND1);
+    %difference between particle x and original x positions
+end
+
+ModifiedTrackingData.deltay=double(CellChange);
+ModifiedTrackingData.deltay(1)=1;
+
+isChangeyIND=[1;find(diff(ModifiedTrackingData.particleid))+1];
+for i=1:(length(isChangeyIND)-1)
+    IND1=isChangeyIND(i);
+    IND2=isChangeyIND(i+1)-1;
+    
+    
+    ModifiedTrackingData.deltay(IND1:IND2)=ModifiedTrackingData.particledetectiony(IND1:IND2)-ModifiedTrackingData.particledetectiony(IND1);
+    %difference between particle y and original y positions
+end
+
+ModifiedTrackingData.deltax(ModifiedTrackingData.deltax==0)=nan;
+delta2x=diff(ModifiedTrackingData.deltax); delta2x(isnan(delta2x))=0; 
+
+ModifiedTrackingData.deltay(ModifiedTrackingData.deltay==0)=nan;
+delta2y=diff(ModifiedTrackingData.deltay); delta2y(isnan(delta2y))=0;
+
+deltaposition=sqrt(delta2x.^2+delta2y.^2);
 
